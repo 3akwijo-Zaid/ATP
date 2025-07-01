960bb40869f104e8608847c6d9a5a5daec92cfe1
Zaid Amirache <zaid.amirache@esi.ac.ma>
2025-07-01 20:09:49 +0100

    z

--- migrations/2024_rename_set_winner_points_to_match_score_points.sql
+++
@@ -1,7 +0,0 @@
--- Migration: Rename set_winner_points to match_score_points in point_settings
-
-ALTER TABLE point_settings ADD COLUMN match_score_points INT DEFAULT 10;
-
-UPDATE point_settings SET match_score_points = set_winner_points;
-
-ALTER TABLE point_settings DROP COLUMN set_winner_points; 
